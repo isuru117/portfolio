054c28ffde0d102d07247e5e5e8965e352ee1732
isuru117 <isuruedirisinghe80@gmail.com>
2024-06-19 12:15:14 +0200

    add scroll container with snapping

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,69 +1,104 @@
 import React from 'react';
-import { Container, Typography, Paper } from '@mui/material';
-import WorkIcon from '@mui/icons-material/Work';
+import { Container, Typography, Paper, Box } from '@mui/material';
 import { TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent, Timeline } from '@mui/lab';
+import logo_99x from '../../assets/logos/99x.png';
 
 const experiences = [
     {
         date: '2020 - Present',
         title: 'Software Engineer',
         company: 'Tech Company',
-        description: 'Developing awesome applications.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        icon: logo_99x
     },
     {
         date: '2018 - 2020',
         title: 'Junior Developer',
         company: 'Startup Inc.',
-        description: 'Worked on various web projects.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        icon: logo_99x
     },
     {
         date: '2016 - 2018',
         title: 'Intern',
         company: 'Big Corp',
         description: 'Assisted in developing internal tools.',
+        icon: logo_99x
+    },
+    {
+        date: '2016 - 2018',
+        title: 'Intern',
+        company: 'Big Corp',
+        description: 'Assisted in developing internal tools.',
+        icon: logo_99x
     },
 ];
 
 const Experience: React.FC = () => {
 
     return (
-        <Container sx={
-            {
-                maxHeight: '80vh',
-                overflowY: 'auto',
-                marginTop: 2,
-                padding: 2
-            }}>
-            <Typography variant="h4" component="h1" gutterBottom>
+        <Container sx={{ height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
+            <Typography variant="h2" gutterBottom>
                 Experience
             </Typography>
-            <Timeline>
-                {experiences.map((experience, index) => (
-                    <TimelineItem key={index}>
-                        <TimelineSeparator>
-                            <TimelineDot color="primary">
-                                <WorkIcon />
-                            </TimelineDot>
-                            {index < experiences.length - 1 && <TimelineConnector />}
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Paper elevation={3} sx={{ padding: 2 }}>
-                                <Typography variant="h6" component="h1">
-                                    {experience.title}
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    {experience.company}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    {experience.date}
-                                </Typography>
-                                <Typography variant="body1">{experience.description}</Typography>
-                            </Paper>
-                        </TimelineContent>
-                    </TimelineItem>
-                ))}
-            </Timeline>
-        </Container >
+            <Box sx={{
+                flexGrow: 1,
+                overflowY: 'auto',
+                padding: 2,
+                scrollSnapType: 'y mandatory',
+                scrollBehavior: 'smooth',  // Enable smooth scrolling
+                '&::-webkit-scrollbar': { display: 'none' },
+                scrollbarWidth: 'none',
+            }}>
+                <Timeline position="alternate">
+                    {experiences.map((experience, index) => (
+                        <TimelineItem
+                            key={index}
+                            sx={{
+                                scrollSnapAlign: 'start',
+                            }}
+                        >
+                            <TimelineSeparator>
+                                <Box
+                                    component="span"
+                                    sx={{
+                                        width: 56,
+                                        height: 56,
+                                        borderRadius: '50%',
+                                        bgcolor: 'background.paper',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        boxShadow: 1,
+                                    }}
+                                >
+                                    <img
+                                        src={experience.icon}
+                                        alt={`${experience.company} logo`}
+                                        style={{ width: 40, height: 40, borderRadius: '50%' }}
+                                    />
+                                </Box>
+                                {index < experiences.length - 1 && <TimelineConnector />}
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ display: 'flex', justifyContent: index % 2 === 0 ? 'flex-start' : 'flex-end' }}>
+                                <Paper elevation={3} sx={{ p: 2, maxWidth: '300px' }}>
+                                    <Typography variant="h6" component="h1">
+                                        {experience.title}
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        {experience.company}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        {experience.date}
+                                    </Typography>
+                                    <Typography variant="body1">{experience.description}</Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+                    ))}
+                </Timeline>
+            </Box>
+        </Container>
     );
 };
 
