859158a9da6df46a8517084f6cdfce35f047ed76
isuru117 <isuruedirisinghe80@gmail.com>
2024-06-19 14:45:32 +0200

    add line breaking for description attribute

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -2,27 +2,45 @@ import React from 'react';
 import { Container, Typography, Box } from '@mui/material';
 import { TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, Timeline } from '@mui/lab';
 import logo_99x from '../../assets/logos/99x.png';
+import logo_pmtc from '../../assets/logos/pmtc.png';
 
 const experiences = [
     {
-        date: '2020 - Present',
+        date: 'Nov. 2022 - Present',
+        title: 'IT Developer (Apprentice)',
+        company: 'Peugeot Motocycles',
+        description: `
+        • Direct and approve the design and implementation of the next-generation mobile connectivity project for two wheelers, in collaboration with global partners in China, India, and Italy.
+        • Ensure high standards of project execution through rigorous management and review processes, enhancing the project’s technological advancement and market readiness.
+        • Brought forward and executed the company’s real-time data logger project, significantly enhancing the efficiency of vehicle diagnostics and monitoring capabilities through improved data collection and analysis.`,
+        technologies: 'Flutter, HERE Maps, AWS, Python, IPEmotion RT',
+        icon: logo_pmtc
+    },
+    {
+        date: 'Jun. 2022 - Aug. 2022',
+        title: 'Senior Software Engineer',
+        company: '99x Technology',
+        description: `
+        • Effectively led Scrum ceremonies as Scrum Master, collaborating closely with Norway-based Product Owners to ensure timely and high-quality product delivery.
+        • Streamlined infrastructure deployment on Azure utilizing ARM templates.
+        • Achieved in developing a compatibility layer between Mapbox GL with React Beautiful DND libraries, meeting client specifications for enhanced drag-and-drop functionalities.`,
+        technologies: 'Azure Functions, Azure DevOps, Azure Resource Manager, .NET Core, TypeScript, React, SQL Server',
+        icon: logo_99x
+    },
+    {
+        date: 'Mar. 2020 - Jun. 2022',
         title: 'Software Engineer',
-        company: 'Tech Company',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        icon: logo_99x
-    },
-    {
-        date: '2018 - 2020',
-        title: 'Junior Developer',
-        company: 'Startup Inc.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        icon: logo_99x
-    },
-    {
-        date: '2016 - 2018',
-        title: 'Intern',
-        company: 'Big Corp',
-        description: 'Assisted in developing internal tools.',
+        company: '99x Technology',
+        description: `
+        • Led the successful design and development of scalable backend REST API services, leveraging ASP.NET Core and
+        MS SQL Server to extend application functionality.
+        • Successfully implemented and integrated authentication middleware across the application using Microsoft Graph API
+        and Azure AD B2C.
+        • Laid the groundwork for the React frontend and enabled Progressive Web Application (PWA) functionality.
+        • Integrated map functionality using Deck GL with MapBox GL to enable storing and retrieving coordinate data from
+        user-drawn polygons along with custom metadata.
+        `,
+        technologies: 'Azure Functions, Azure AD, Azure DevOps, .NET Core, TypeScript, React, SQL Server',
         icon: logo_99x
     },
     {
@@ -30,9 +48,20 @@ const experiences = [
         title: 'Intern',
         company: 'Big Corp',
         description: 'Assisted in developing internal tools.',
+        technologies: 'Flutter, HERE Maps, AWS, Python, IPEmotion RT',
         icon: logo_99x
     },
 ];
+
+const ExperienceDescription = ({ description }: { description: string }) => {
+    const paragraphs = description.split('\n').map((paragraph, index) => (
+        <Typography key={index} variant="body1" style={{ marginBottom: '8px' }}>
+            {paragraph}
+        </Typography>
+    ));
+
+    return <>{paragraphs}</>;
+};
 
 const Experience: React.FC = () => {
 
@@ -46,7 +75,7 @@ const Experience: React.FC = () => {
                 overflowY: 'auto',
                 padding: 2,
                 scrollSnapType: 'y mandatory',
-                scrollBehavior: 'smooth',  // Enable smooth scrolling
+                scrollBehavior: 'smooth',
                 '&::-webkit-scrollbar': { display: 'none' },
                 scrollbarWidth: 'none',
             }}>
@@ -81,7 +110,7 @@ const Experience: React.FC = () => {
                                 {index < experiences.length - 1 && <TimelineConnector />}
                             </TimelineSeparator>
                             <TimelineContent sx={{ display: 'flex', justifyContent: index % 2 === 0 ? 'flex-start' : 'flex-end' }}>
-                                <div style={{ padding: 2, maxWidth: '300px' }}>
+                                <div style={{ padding: 2, maxWidth: '600px' }}>
                                     <Typography variant="h6" component="h1">
                                         {experience.title}
                                     </Typography>
@@ -91,7 +120,10 @@ const Experience: React.FC = () => {
                                     <Typography variant="body2" color="textSecondary">
                                         {experience.date}
                                     </Typography>
-                                    <Typography variant="body1">{experience.description}</Typography>
+                                    <ExperienceDescription description={experience.description} />
+                                    <Typography variant="body2" color="textSecondary">
+                                        Technologies: {experience.technologies}
+                                    </Typography>
                                 </div>
                             </TimelineContent>
                         </TimelineItem>
