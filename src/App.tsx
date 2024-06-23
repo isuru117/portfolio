3799a7d11005bd64300b79c77f24c314a54e8ed7
isuru117 <isuruedirisinghe80@gmail.com>
2024-06-23 08:12:47 +0200

    scroll to top fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import './App.css';
 const App: React.FC = () => {
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.history.scrollRestoration = 'manual';
   }, []);
 
   const Section: React.FC<any> = ({ id, children }) => {
